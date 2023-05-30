f828cde7b85e87ed0388edc2c6c011e8be6d6953
GGeetha123 <geetha_ind@yahoo.com>
2023-05-30 11:08:48 +0530

    'changed'

--- StaticWebSite/index.js
+++ StaticWebSite/index.js
@@ -1,9 +1,23 @@
-images=["first.jpg","second.jpeg","third.jpeg","fourth.jpeg",
-"fifth.jpeg",
-"sixth.jpg"]
+Idx = 0
+images=["first.jpg","second.jpg","third.jpg","fourth.jpg",
+"fifth.jpg","sixth.jpg"]
 
 setInterval(
     function() {
-     document.getElementById("img1").src = images[ Math.round(Math.random()*5)]
-    },2000
+        Idx =  Math.round(Math.random()*5)
+     document.getElementById("img1").src = images[Idx]
+    },10000
 )
+
+function changeImage(arrowType)
+{
+if(arrowType == "right")
+{
+    Idx = Idx==5?0:++Idx
+}
+if(arrowType == "left")
+{
+    Idx = Idx==5?0:--Idx
+}
+document.getElementById("img1").src = images[Idx]
+}
